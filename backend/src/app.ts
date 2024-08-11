17ffd34c92026bee64a1cc59b7ea69df7bc8ecee
goyalrachit15 <122667317+goyalrachit15@users.noreply.github.com>
2024-08-12 00:55:20 +0530

    cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://task-2-yl6f.onrender.com/',
   credentials: true, 
 }));
 connectDB();
